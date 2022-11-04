3f3fc2b158424bc151eed02b38049094025538da
2190153 <116859453+2190153@users.noreply.github.com>
2022-11-04 11:44:41 +0800

    Add files via upload

---
+++ Square.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class Square: Rectangle
+    {
+        public new double Width
+        {
+            get { return base.Width; }
+            set { base.Width = value;
+                base.Height = value;
+            }
+        }
+        public new double Height
+        {
+            get { return base.Height; }
+            set { base.Height = value;
+                base.Height = value;
+            }
+        }
+
+        public double len
+        {
+            get { return base.Height; }
+            set
+            {
+                base.Height = value;
+                base.Width = value;
+            }
+        }
+
+        public Square(): base()
+        {
+
+        }
+
+        public Square(double len): base(len, len)
+        {
+
+        }
+    }
+}
